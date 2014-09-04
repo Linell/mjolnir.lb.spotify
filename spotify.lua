f69c2423f0553208b54d3e3706784150b668eaec
Linell Bonnette <tlbonnette@gmail.com>
2014-09-04 11:49:23 -0500

    Documentation and README

--- spotify.lua
+++ spotify.lua
@@ -1,45 +1,62 @@
-local spotify = {}
--- If you don't have a C or Objective-C submodule, the above line gets simpler:
--- local foobar = {}
+--- === mjolnir.lb.spotify ===
+--
+-- Controls for Spotify music player
+--
+-- Usage: local spotify = require "mjolnir.lb.spotify"
 
--- If your module depends on other Mjolnir modules, require them into locals like this:
+local spotify = {}
+
 local application = require "mjolnir.application"
 local alert = require "mjolnir.alert"
 
--- Simple functions that can be defined in Lua, should be defined in Lua:
-function spotify.tell(cmd)
+-- Internal function to pass a command to Applescript.
+local function tell(cmd)
   return os.execute('osascript -e \'tell application "Spotify" to ' .. cmd .. "'")
 end
 
+--- mjolnir.lb.spotify.play() -> nil
+--- Function
+--- Toggles play/pause of current Spotify track
 function spotify.play()
-  spotify.tell('playpause')
+  tell('playpause')
   alert.show(' ▶', 0.5)
 end
+--- mjolnir.lb.spotify.pause() -> nil
+--- Function
+--- Pauses of current Spotify track
 function spotify.pause()
-  spotify.tell('pause')
+  tell('pause')
   alert.show(' ◼', 0.5)
 end
+--- mjolnir.lb.spotify.next() -> nil
+--- Function
+--- Skips to the next Spotify track
 function spotify.next()
-  spotify.tell('next track')
+  tell('next track')
 end
+--- mjolnir.lb.spotify.previous() -> nil
+--- Function
+--- Skips to previous Spotify track
 function spotify.previous()
-  spotify.tell('previous track')
+  tell('previous track')
 end
 
-function spotify.get(cmd)
+-- Internal command used to pass a command and get back output from the command.
+local function get(cmd)
   local handle = io.popen('osascript -e \'tell application "Spotify" to ' .. cmd .. "'")
   local result = handle:read("*a")
   handle:close()
   return result
 end
 
+--- mjolnir.lb.spotify.displayCurrentTrack() -> nil
+--- Function
+--- Displays information for current track
 function spotify.displayCurrentTrack()
-  artist = spotify.get('get the artist of the current track')
-  album  = spotify.get('get the album of the current track')
-  track  = spotify.get('get the name of the current track')
+  artist = get('get the artist of the current track')
+  album  = get('get the album of the current track')
+  track  = get('get the name of the current track')
   alert.show(track .. album .. artist, 1)
 end
 
-
--- Always return your top-level module; never set globals.
 return spotify
